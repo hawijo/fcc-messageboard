44dcb86de13fdb1c7ad8186a7d1c5d166e447307
Joshua <joshuahawi24@gmail.com>
2025-03-05 14:08:13 +0300

    Security Finished and mongodb Uri Added

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,12 @@
 'use strict';
 
-module.exports = function (app) {
-  
+let mongodb = require('mongodb')
+let mongoose = require('mongoose')
+
+module.exports = function (app) { 
+  let uri = 'mongodb+srv://joshuahawi24:' + process.env.PW + '@w3-tutorial.q90bo.mongodb.net/message_board?retryWrites=true&w=majority&appName=W3-Tutorial'
+	mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+
   app.route('/api/threads/:board');
     
   app.route('/api/replies/:board');
